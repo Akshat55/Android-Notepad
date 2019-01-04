b18978840073747a852dffb5313efbfc0d06cd88
Akshat55 <38994122+Akshat55@users.noreply.github.com>
2019-01-04 16:53:04 -0500

    Removed the ActionBar (Toolbar) from all of the layouts and created a custom Toolbar. Also, created the XML layout for the Note Activity.

--- app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
+++ app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
@@ -1,8 +1,10 @@
 package ca.akshatpatel.notepad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
@@ -13,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //Sets the new toolbar
         Toolbar toolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(toolbar);
@@ -22,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         // Retrieve attributes from layout
         ImageButton newEntry = findViewById(R.id.add);
         ListView myList = findViewById(R.id.notepad_listview);
+
+
+        //Set on click listener which starts the NoteActivity activity
+        newEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startNoteActivity = new Intent(MainActivity.this, NoteActivity.class);
+                startActivity(startNoteActivity);
+            }
+        });
     }
 }
--- app/src/main/java/ca/akshatpatel/notepad/NoteActivity.java
+++ app/src/main/java/ca/akshatpatel/notepad/NoteActivity.java
@@ -2,12 +2,17 @@ package ca.akshatpatel.notepad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class Note extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        //Set the toolbar which allows saving
+        Toolbar toolbar = findViewById(R.id.note_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
