77477b0b4200fa048e2fb2690b9154a707c15297
Akshat55 <38994122+Akshat55@users.noreply.github.com>
2019-01-04 02:45:04 -0500

    Created Main Layout & The Note Layout which will handle the CRU functionality.

--- app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
+++ app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
@@ -2,6 +2,9 @@ package ca.akshatpatel.notepad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Sets the new toolbar
+        Toolbar toolbar = findViewById(R.id.custom_toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // Retrieve attributes from layout
+        ImageButton newEntry = findViewById(R.id.add);
+        ListView myList = findViewById(R.id.notepad_listview);
     }
 }
