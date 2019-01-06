b062e4dc6bcf28052fdd797c6a51bcb928042f3a
Akshat55 <38994122+Akshat55@users.noreply.github.com>
2019-01-06 00:16:09 -0500

    Added the delete functionality from main activity (onItemLongClick)

--- app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
+++ app/src/main/java/ca/akshatpatel/notepad/MainActivity.java
@@ -3,7 +3,10 @@ package ca.akshatpatel.notepad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.custom_toolbar);
         setSupportActionBar(toolbar);
 
-        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+        final DatabaseHandler db = new DatabaseHandler(getApplicationContext());
         notes = db.getAllNotes();
 
         // Retrieve attributes from layout & set adapter for ListView
@@ -48,13 +51,52 @@ public class MainActivity extends AppCompatActivity {
         myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent updateNoteActivity = new Intent(MainActivity.this, NoteActivity.class);
-                updateNoteActivity.putExtra("option", "update");
-                updateNoteActivity.putExtra("id", notes.get(position).getId());
-                updateNoteActivity.putExtra("name", notes.get(position).getName());
-                updateNoteActivity.putExtra("message", notes.get(position).getMessage());
-                startActivity(updateNoteActivity);
+                sendEditIntent(position);
             }
         });
+
+
+        myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+                popup.getMenuInflater().inflate(R.menu.note_select_menu, popup.getMenu());
+                popup.setGravity(Gravity.END);
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        //Gets selected title
+
+                        switch (item.getItemId()) {
+                            case R.id.menuEdit:
+                                sendEditIntent(position);
+                                break;
+                            case R.id.menuDelete:
+
+                                db.deleteNote(notes.get(position).getId());
+                                notes.remove(position);
+                                break;
+                            default:
+                                break;
+                        }
+
+                        return false;
+                    }
+                });
+                popup.show();
+                return true;
+            }
+        });
+
+
+    }
+
+    private void sendEditIntent(int position) {
+        Intent updateNoteActivity = new Intent(MainActivity.this, NoteActivity.class);
+        updateNoteActivity.putExtra("option", "update");
+        updateNoteActivity.putExtra("id", notes.get(position).getId());
+        updateNoteActivity.putExtra("name", notes.get(position).getName());
+        updateNoteActivity.putExtra("message", notes.get(position).getMessage());
+        startActivity(updateNoteActivity);
     }
 }
